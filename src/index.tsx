30ddf598f78b2b8dd0494ebcdd5543d7be1a5493
shota-nukumizu <author2000.1225@gmail.com>
2022-03-25 07:13:57 +0900

    add album and router

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import Album from './Album';
 import theme from './theme';
+import { Route, BrowserRouter } from "react-router-dom";
 
 
 ReactDOM.render(
@@ -12,5 +14,18 @@ ReactDOM.render(
     <CssBaseline />
     <App />
   </ThemeProvider>,
+  document.querySelector('#root'),
+);
+
+// Router Setting
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <div>
+        <Route path="/" element={App} />
+        <Route path="/album" element={Album} />
+      </div>
+    </BrowserRouter>
+  </React.StrictMode>,
   document.querySelector('#root'),
 );
